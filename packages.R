ce92eff25b644f11dcf3369232f4a4ac11553b83
Jay Gillenwater <jhgille2@ncsu.edu>
2023-02-15 12:27:24 -0500

    Initial upload

---
+++ packages.R
@@ -0,0 +1,17 @@
+# Check if pacman is installed and install it if it is not
+list.of.packages <- c("pacman")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+# Workflow organization
+pacman::p_load(conflicted, 
+dotenv, 
+targets)
+
+# Data wrangling
+pacman::p_load(dplyr, 
+tidyr, 
+janitor)
+
+# Analysis
+pacman::p_load(qtl)
