24b4284bb3d67bdba54142512277cc2c7c5c1e3e
Jay Gillenwater <55106933+jhgille2@users.noreply.github.com>
2023-05-24 11:40:50 -0400

    Update do_stepwise_mapping.R
    Corrected an error where the index of the phenotype in the permutations dataframe did not match the index of the phenotype in the cross "pheno" component.

--- R/do_stepwise_mapping.R
+++ R/do_stepwise_mapping.R
@@ -11,22 +11,26 @@
 #' @export
 do_stepwise_mapping <- function(cross, cross_permutations, pheno, sig_alpha) {
 
-      # First, get the phenotype column number and the penalties for the phenotype
-    pheno_col_number <- str_detect(phenames(cross), paste0("\\b", pheno, "\\b")) %>% which()
-    pheno_penalties  <- calc.penalties(cross_permutations, 
-                                       lodcolumn = pheno_col_number,
-                                       alpha     = sig_alpha)
+    # First, get the phenotype column number and the penalties for the phenotype
+    pheno_col_number <- str_detect(phenames(simulated_cross), paste0("\\b", pheno, "\\b")) %>% which()
+    
+    perm_col_names <- cross_permutations %>% 
+      purrr::pluck("full") %>% 
+      colnames()
+    
+    perm_col_index <- str_detect(perm_col_names, paste0("\\b", pheno, "\\b")) %>% which()
+    
+    pheno_penalties  <- calc.penalties(cross_permutations, lodcolumn = perm_col_index)
 
     simulated_cross <- calc.genoprob(simulated_cross)
 
     # Perform stepwise mapping on the given phenotype
-    stepwise_result <- stepwiseqtl(cross            = simulated_cross, 
-                                   pheno.col        = pheno_col_number, 
-                                   method           = "hk", 
-                                   penalties        = pheno_penalties, 
-                                   keeptrace        = TRUE, 
-                                   refine.locations = TRUE,
-                                   additive.only    = TRUE)
+    stepwise_result <- stepwiseqtl(cross     = simulated_cross, 
+                                   pheno.col = pheno, 
+                                   method    = "hk", 
+                                   penalties = pheno_penalties, 
+                                   keeptrace = TRUE, 
+                                   refine.locations = TRUE)
 
     # Return the result of the mapping
     return(stepwise_result)
