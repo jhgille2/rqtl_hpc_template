cf540e11982ca9ca9c9b1ce86bc32b6b6a17f880
Jay Gillenwater <jhgille2@ncsu.edu>
2023-06-08 11:31:04 -0400

    updates to batch script and also added a cross csv file that includes data from CLA

--- job.sh
+++ job.sh
@@ -4,16 +4,18 @@
 
 # Modified from https://github.com/mschubert/clustermq/blob/master/inst/LSF.tmpl
 # under the Apache 2.0 license:
-#SBATCH --job-name=rqtl_template_test
+#SBATCH --job-name=raleigh_soja_qtl_mapping
 #SBATCH --output=/dev/null
 #SBATCH --error=/dev/null
 #SBATCH --mem-per-cpu=3000
 #SBATCH --cpus-per-task=1
-#SBATCH -N 1
-#SBATCH -n 5
-#SBATCH -t 1:00:00
+#SBATCH -N 4
+#SBATCH -n 287
+#SBATCH -t 40:00:00
 #SBATCH --mail-user=jhgille2@ncsu.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --exclusive
+#SBATCH -p=short
 
 module load R # Comment out if R is not an environment module.
 module load miniconda
